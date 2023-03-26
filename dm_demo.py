57767dc6f244491a3ef4703d6fef2b9e78a29d44
kiril <kirill@pheon.com>
2023-03-26 16:20:18 +0200

    change pipeline

--- dm_demo.py
+++ dm_demo.py
@@ -2,7 +2,24 @@
     Script to demonstrate the use of stable diffusion model.
 """
 import torch
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+from PIL import Image
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler, StableDiffusionImg2ImgPipeline
+
+
+def image_grid(imgs, rows=2, cols=2):
+    """Create a grid of images.
+
+    :param imgs: list of PIL images
+    :param rows: number of rows
+    :param cols: number of columns
+    """
+    w, h = imgs[0].size
+    grid = Image.new('RGB', size=(cols * w, rows * h))
+
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i % cols * w, i // cols * h))
+    return grid
+
 
 model_id = "stabilityai/stable-diffusion-2-1"
 
@@ -10,12 +27,17 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
 # Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
-sd_dtype = torch.float32 if device == "cpu" else torch.float16
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=sd_dtype)
+sd_dtype = torch.bfloat16 if device == "cpu" else torch.float16
+pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id, torch_dtype=sd_dtype)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to(device)
 
-prompt = "a photo of an astronaut riding a horse on the moon during a comet storm"
-image = pipe(prompt).images[0]
+prompt = [""] * 4
 
-image.save("astronaut_rides_horse.png")
+# loading image with PIL
+image = Image.open("the_cat.png")
+
+images = pipe(prompt=prompt, image=image).images
+grid = image_grid(images, rows=2, cols=2)
+
+grid.save("remix.png")
